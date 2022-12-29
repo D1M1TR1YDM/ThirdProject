3d5da7da121cc94951efd601f5140475687ab427
D1M1TR1YDM <dimaharcenko513@gmail.com>
2022-12-29 20:04:03 +0600

    calculater

--- src/Main.java
+++ src/Main.java
@@ -8,49 +8,16 @@ import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        FileWriter fileWriter = new FileWriter("Authorization.txt");
-        FileReader fileReader = new FileReader("Authorization.txt");
-        Scanner reader = new Scanner(fileReader);
-
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String pw = "";
-        boolean checkPassword = false;
-        do {
-            System.out.println("write your new login");
-            String logIn = scanner.nextLine();
-            System.out.println("write your  new password");
-            String password = scanner.nextLine();
-            System.out.println("confirm your password");
-            String cPassword = scanner.nextLine();
-            if (Objects.equals(password, cPassword)) {
-                System.out.println("you have registred");
-                fileWriter.write(logIn + "\n");
-                fileWriter.write(password);
-                fileWriter.close();
-                checkPassword = true;
 
-            } else System.out.println("wrong password");
-        } while (!checkPassword);
+        Calculator calculator = new Calculator(0, scanner);
+        calculator.process();
 
-        checkPassword = false;
-        do {
-            System.out.println("write your login to authorize");
-            String logIn = scanner.nextLine();
-            System.out.println("your password");
-            String password = scanner.nextLine();
-            while (reader.hasNextLine()) {
-                pw = reader.nextLine();
-                System.out.println(pw);
-            }
-            reader.close();
-            if (!Objects.equals(reader.nextLine(), logIn) || !Objects.equals(pw, password)) {
-                System.out.println("wrong login or password try again");
-            } else {
-                System.out.println("you have logged in");
-                checkPassword = true;
-            }
-        } while (!checkPassword);
+
     }
+
+
+}
 
 }
